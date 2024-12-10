23dc780348d0964b1b81550ba93de9ded89780d6
Nicolò Penserini <nicolo.penserini@studio.unibo.it>
2024-12-10 16:05:36 +0100

    Removed classes from CSS

--- html/index.js
+++ html/index.js
@@ -1,4 +1,4 @@
-document.querySelector(".list-menù").addEventListener("click", function () {
+document.querySelector("body>header>nav>img:nth-child(1)").addEventListener("click", function () {
     console.log("cliccato");
     window.location.href = "Menu/indexMenu.html";
   });
