76630c944c6e89a7ab2f5d40dcb032573395a1a3
Alex Frisoni <alex.frisoni2@studio.unibo.it>
2024-12-10 15:30:24 +0100

    renamed menu index

--- html/index.js
+++ html/index.js
@@ -1,5 +1,4 @@
 document.querySelector(".list-menù").addEventListener("click", function () {
-    // Specifica l'URL della nuova pagina
     console.log("cliccato");
-    window.location.href = "Menu/index.html";
+    window.location.href = "Menu/indexMenu.html";
   });
