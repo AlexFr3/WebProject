8e4e39fa01d22f86d5f9a44c6f2abc2afff15293
Alex Frisoni <alex.frisoni2@studio.unibo.it>
2024-12-11 14:51:46 +0100

    Added navbar

--- html/index.js
+++ html/index.js
@@ -1,4 +1,16 @@
 document.querySelector("body>header>nav>img:nth-child(1)").addEventListener("click", function () {
-    console.log("cliccato");
-    window.location.href = "Menu/indexMenu.html";
-  });
+  console.log("cliccato");
+  window.location.href = "menu.html";
+});
+document.querySelector("body>header>nav>img:nth-child(1)").addEventListener("click", function () {
+  console.log("cliccato");
+  window.location.href = "profile.html";
+});
+document.querySelector("body>header>nav>img:nth-child(1)").addEventListener("click", function () {
+  console.log("cliccato");
+  window.location.href = "cart.html";
+});
+document.querySelector("body>header>nav>img:nth-child(1)").addEventListener("click", function () {
+  console.log("cliccato");
+  window.location.href = "manga.html";
+});
