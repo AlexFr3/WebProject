9892f6a6f4069ed0c40f59c296ecff54f0095eee
Alex Frisoni <alex.frisoni2@studio.unibo.it>
2025-01-02 00:48:18 +0100

    Added dinamic manga

--- php/index.php
+++ php/index.php
@@ -2,6 +2,10 @@
 require_once 'bootstrap.php';
 $templateParams["titolo"] = "MangaParadise | Home";
 //$templateParams["js"] = array("js/index.js");
+$templateParams["RandomManga"] = $dbh->getRandomManga(6);
+$templateParams["LatestManga"] = $dbh->getLatestManga(6);
+$templateParams["TopRatedManga"] = $dbh->getTopRatedManga(3);
+
 $templateParams["nome"] = "menu.php";
 require 'template/base.php';
 ?>
--- php/template/menu.php
+++ php/template/menu.php
@@ -10,95 +10,40 @@
       <a href="#slide-2"></a>
     </div>
   </div>
-</section>
-
-<section>
+</section><section>
   <h1>Ultime Uscite</h1>
   <ul>
-    <li>
-      <article>
-        <img src="../img/Manga/minecraft_vol_1.jpg" alt="Minecraft Volume 1" />
-        <h2>Minecraft Vol. 1</h2>
-        <p>5,99€</p>
-        <input type="button" value="Aggiungi al carrello" />
-      </article>
-    </li>
-    <li>
-      <article>
-        <img src="../img/Manga/dandadan_vol_1.jpg" alt="Dandadan 1" />
-        <h2>Dandadan Vol. 1</h2>
-        <p>5,99€</p>
-        <input type="button" value="Aggiungi al carrello" />
-      </article>
-    </li>
-    <li>
-      <article>
-        <img src="../img/Manga/hunter_x_hunter_vol_1.jpg" alt="Hunter x Hunter Volume 1" />
-        <h2>Hunter x Hunter Vol. 1</h2>
-        <p>5,99€</p>
-        <input type="button" value="Aggiungi al carrello" />
-      </article>
-    </li>
-    <li>
-      <article>
-        <img src="../img/Manga/detective_conan_vol_1.jpg" alt="Detective conan Volume 1" />
-        <h2>Conan Vol. 1</h2>
-        <p>5,99€</p>
-        <input type="button" value="Aggiungi al carrello" />
-      </article>
-    </li>
-    <li>
-      <article>
-        <img src="../img/Manga/naruto_vol_2.jpg" alt="Naruto Volume 2" />
-        <h2>Naruto Vol. 2</h2>
-        <p>5,99€</p>
-        <input type="button" value="Aggiungi al carrello" />
-      </article>
-    </li>
-    <li>
-      <article>
-        <img src="../img/Manga/one_piece_vol_1.jpg" alt="One Piece Volume 1" />
-        <h2>One Piece Vol. 1</h2>
-        <p>5,99€</p>
-        <input type="button" value="Aggiungi al carrello" />
-      </article>
-    </li>
-    <li>
-      <article>
-        <img src="../img/Manga/one_piece_vol_2.png" alt="One Piece Volume 2" />
-        <h2>One Piece Vol. 2</h2>
-        <p>5,99€</p>
-        <input type="button" value="Aggiungi al carrello" />
-      </article>
-    </li>
+    <?php if (!empty($templateParams["LatestManga"])): ?>
+      <?php foreach ($templateParams["LatestManga"] as $manga): ?>
+        <li>
+          <article>
+            <img src="../img/Manga/<?= htmlspecialchars($manga['Immagine']) ?>" alt="<?= htmlspecialchars($manga['Titolo']) ?>" />
+            <h2><?= htmlspecialchars($manga['Titolo']) ?></h2>
+            <input type="button" value="Aggiungi al carrello" />
+          </article>
+        </li>
+      <?php endforeach; ?>
+    <?php else: ?>
+      <li>Nessun manga disponibile al momento.</li>
+    <?php endif; ?>
   </ul>
 </section>
 <section>
   <h1>I più votati</h1>
   <ul>
-    <li>
-      <article>
-        <img src="../img/Manga/naruto_vol_1.jpg" alt="Naruto Volume 1" />
-        <h2>Naruto Vol. 1</h2>
-        <p>5,99€</p>
-        <input type="button" value="Aggiungi al carrello" />
-      </article>
-    </li>
-    <li>
-      <article>
-        <img src="../img/Manga/one_piece_vol_1.jpg" alt="One Piece Volume 1" />
-        <h2>One Piece Vol. 1</h2>
-        <p>5,99€</p>
-        <input type="button" value="Aggiungi al carrello" />
-      </article>
-    </li>
-    <li>
-      <article>
-        <img src="../img/Manga/detective_conan_vol_1.jpg" alt="Detective conan Volume 1" />
-        <h2>Conan Vol. 1</h2>
-        <p>5,99€</p>
-        <input type="button" value="Aggiungi al carrello" />
-      </article>
-    </li>
+    <?php if (!empty($templateParams["TopRatedManga"])): ?>
+      <?php foreach ($templateParams["TopRatedManga"] as $manga): ?>
+        <li>
+          <article>
+            <img src="../img/Manga/<?= htmlspecialchars($manga['Immagine']) ?>" alt="<?= htmlspecialchars($manga['Titolo']) ?>" />
+            <h2><?= htmlspecialchars($manga['Titolo']) ?></h2>
+            <p>Voto: <?= htmlspecialchars($manga['Voto']) ?>/10</p>
+            <input type="button" value="Aggiungi al carrello" />
+          </article>
+        </li>
+      <?php endforeach; ?>
+    <?php else: ?>
+      <li>Nessun manga disponibile al momento.</li>
+    <?php endif; ?>
   </ul>
 </section>
