46b49da84cd8a46b4f8f2d28ef66dbd3cd47f2a9
Nicolò Penserini <nicolo.penserini@studio.unibo.it>
2025-01-10 17:21:46 +0100

    Removed comment from logout.php

--- php/logout.php
+++ php/logout.php
@@ -1,24 +1,6 @@
 <?php
 require_once 'bootstrap.php';
 
-/*
-if(isset($_POST["email"]) && isset($_POST["password"])){
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-    if(!$dbh->existsUserWithEmail($email)){
-        //account non esistente
-        $templateParams["errorelogin"] = "Errore! Non è stato possibile trovare un utente con questa mail!";
-    } else{
-        $login_result = $dbh->checkLogin($email, $password);
-        if(count($login_result)==0){
-            //password errata
-            $templateParams["errorelogin"] = "Errore! La password non è corretta!";
-        } else{
-            registerLoggedUser($login_result[0]);
-        }
-    }
-}
-*/
 if(!isUserLoggedIn()){
     header("location: profilo.php");
 } else{
