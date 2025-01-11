2b8c9b3db9c9d28ef3323337221183d4cf5f684b
Alex Frisoni <alex.frisoni2@studio.unibo.it>
2025-01-11 11:35:30 +0100

    added shopping cart to database

--- SQL/MangaParadiseDatabase.sql
+++ SQL/MangaParadiseDatabase.sql
@@ -52,3 +52,13 @@ CREATE TABLE IF NOT EXISTS Manga_has_Categoria (
     FOREIGN KEY (Categoria_idCategoria) REFERENCES Categoria(idCategoria),
     PRIMARY KEY (Manga_idManga, Categoria_idCategoria)
 );
+
+-- Creazione della tabella Carrello
+CREATE TABLE IF NOT EXISTS Carrello (
+    idCarrello INT(11) AUTO_INCREMENT PRIMARY KEY, 
+    Utente_Email VARCHAR(100) NOT NULL,             
+    Manga_idManga INT(11) NOT NULL,                 
+    Quantità INT(11) NOT NULL DEFAULT 1,            
+    FOREIGN KEY (Utente_Email) REFERENCES Utente(Email),
+    FOREIGN KEY (Manga_idManga) REFERENCES Manga(idManga)
+);
