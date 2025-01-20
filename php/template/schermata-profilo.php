8682925db6fdd14dc33c89883c93676b982b59a4
Alex Frisoni <alex.frisoni2@studio.unibo.it>
2025-01-20 18:15:02 +0100

    added .profile-buttom

--- php/template/schermata-profilo.php
+++ php/template/schermata-profilo.php
@@ -1,2 +1,9 @@
-<p>Bentornato, <?php echo($_SESSION["nome"]); ?> <?php echo($_SESSION["cognome"]); ?></p>
-<a class="red-button" href="logout.php" id="logout">Logout</a>
+<p>Bentornato, <?php echo ($_SESSION["nome"]); ?> <?php echo ($_SESSION["cognome"]); ?></p>
+
+<p><strong>Nome:</strong> <?php echo htmlspecialchars($_SESSION["nome"]); ?></p>
+<p><strong>Cognome:</strong> <?php echo htmlspecialchars($_SESSION["cognome"]); ?></p>
+<p><strong>Email:</strong> <?php echo htmlspecialchars($_SESSION["email"]); ?></p>
+<p><a class="profile-button" href="order.php" id="order">I miei ordini</a></p>
+<p><a class="profile-button" href="notifiche.php" id="notifiche">Notifiche</a></p>
+
+<p><a class="red-button" href="logout.php" id="logout">Logout</a></p>
