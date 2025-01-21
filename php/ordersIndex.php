b0e125815e1577a8e3ca55bf2c81aa33f5b15fd6
Alex Frisoni <alex.frisoni2@studio.unibo.it>
2025-01-21 17:42:08 +0100

    fixed orders

--- php/ordersIndex.php
+++ php/ordersIndex.php
@@ -1,12 +1,11 @@
 <?php
-require_once 'bootstrap.php';
 if (!isUserLoggedIn()) {
     header("location: login.php");
 } else {
     $templateParams["titolo"] = "MangaParadise | I miei ordini";
     $templateParams["nome"] = "orders.php";
     $templateParams["scripts"] = ["darkMode.js", "orders.js"];  
-    /*$templateParams["orders"] = $dbh->getOrdersByUser($_SESSION["email"]);*/
+    $templateParams["orders"] = $dbh->getOrdersByUser($_SESSION["email"]);
     require 'template/base.php';
 }
 ?>
--- php/template/orders.php
+++ php/template/orders.php
@@ -1,13 +1,8 @@
-<?php 
-if(empty($templateParams["orders"])) {
-    echo "<p>Nessun ordine trovato.</p>";
-} else {
-    foreach($templateParams["orders"] as $ordine): ?>
-        <section class="order-section">
-            <h3>Ordine #<?php echo $ordine['idOrdine']; ?></h3>
-            <p><strong>Data Ordine:</strong> <?php echo $ordine['Data_ordine']; ?></p>
-            <p><strong>Stato:</strong> <?php echo $ordine['Stato']; ?></p>
-        </section>
-    <?php endforeach;
-}
-?>
+<?php foreach($templateParams["orders"] as $ordine): ?>
+    <section class="order-section">
+        <h3>Ordine #<?php echo $ordine['idOrdine']; ?></h3>
+        <p><strong>Data Ordine:</strong> <?php echo $ordine['Data_ordine']; ?></p>
+        <!-- <p><strong>Stato:</strong> <?php echo $ordine['Stato']; ?></p>-->
+        <p><strong>Totale:</strong> €<?php echo $ordine['Totale']; ?></p>
+    </section>
+<?php endforeach; ?>
