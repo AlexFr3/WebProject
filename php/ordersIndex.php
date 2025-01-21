6579975c78ed62c36c7611d6c2da58e70b68bffe
Alex Frisoni <alex.frisoni2@studio.unibo.it>
2025-01-21 23:08:45 +0100

    Removed unnecessary script

--- php/ordersIndex.php
+++ php/ordersIndex.php
@@ -5,7 +5,7 @@ if (!isUserLoggedIn()) {
 } else {
     $templateParams["titolo"] = "MangaParadise | I miei ordini";
     $templateParams["nome"] = "orders.php";
-    $templateParams["scripts"] = ["darkMode.js", "orders.js"];  
+    $templateParams["scripts"] = ["darkMode.js"];  
     $templateParams["orders"] = $dbh->getOrdersByUser($_SESSION["email"]);
     require 'template/base.php';
 }
