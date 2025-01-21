f8a1bc801c960eb48b0080fabe2ee301dc66912b
Alex Frisoni <alex.frisoni2@studio.unibo.it>
2025-01-21 15:33:00 +0100

    modified control manga order

--- php/template/shoppingCart.php
+++ php/template/shoppingCart.php
@@ -1,13 +1,15 @@
-<?php if(isset($templateParams["messaggiocarrello"])): ?>
-            <div class="alert text-center <?php if(str_starts_with($templateParams["messaggiocarrello"], "L") || str_starts_with($templateParams["messaggiocarrello"], "G")){
-                echo("alert-success");
-             } else{
-                echo("alert-danger");
-             }?>" role="alert"><p class="fw-bold fs-2" ><?php echo $templateParams["messaggiocarrello"]; ?></p></div>
+<?php if (isset($templateParams["messaggiocarrello"])): ?>
+  <div class="alert text-center <?php if (str_starts_with($templateParams["messaggiocarrello"], "L") || str_starts_with($templateParams["messaggiocarrello"], "G")) {
+    echo ("alert-success");
+  } else {
+    echo ("alert-danger");
+  } ?>" role="alert">
+    <p class="fw-bold fs-2"><?php echo $templateParams["messaggiocarrello"]; ?></p>
+  </div>
 <?php endif; ?>
 <section>
-   <h1>Carrello</h1>
-   <ul>
+  <h1>Carrello</h1>
+  <ul>
     <?php if (!empty($templateParams["manga-in-carrello"])): ?>
       <?php foreach ($templateParams["manga-in-carrello"] as $manga): ?>
         <li>
@@ -16,13 +18,17 @@
               alt="<?= htmlspecialchars($manga['Titolo']) ?>" />
             <h2><?= htmlspecialchars($manga['Titolo']) ?></h2>
             <p>Prezzo: €<?= number_format($manga['Prezzo'], 2) ?></p>
-            <p>Quantità: <?php echo($manga["Quantità_In_Carrello"]) ?></p>
-            <input type="hidden" value="<?php echo($manga['idManga']) ?>" />
-            <input class="button-manga button-manga-quantity decrease-quantity" type="button" value="-" <?php if($manga["Quantità_In_Carrello"] <= 1){ echo("disabled"); } ?>/>
-            <input type="hidden" value="<?php echo($manga['idManga']) ?>" />
-            <input class="button-manga remove-from-cart" type="button" value="✖" />
-            <input type="hidden" value="<?php echo($manga['idManga']) ?>" />
+            <p>Quantità: <?php echo ($manga["Quantità_In_Carrello"]) ?></p>
+            <input type="hidden" value="<?php echo ($manga['idManga']) ?>" />
+            <input class="button-manga button-manga-quantity decrease-quantity" type="button" value="-" <?php if ($manga["Quantità_In_Carrello"] <= 1) {
+              echo ("disabled");
+            } ?> />
+            <input type="hidden" value="<?php echo ($manga['idManga']) ?>" />
             <input class="button-manga button-manga-quantity increase-quantity" type="button" value="+" />
+            <section>
+              <input type="hidden" value="<?php echo ($manga['idManga']) ?>" />
+              <input class="button-manga remove-from-cart" type="button" value="✖" />
+            </section>
           </article>
         </li>
       <?php endforeach; ?>
@@ -31,7 +37,8 @@
   <a class="red-button" href="emptyCart.php">Svuota il carrello</a>
 </section>
 <section>
-   <h1>Riepilogo</h1>
-   <p class="lead fw-bold"> Numero Articoli: <?php echo($templateParams["numero-articoli"]) ?><br/>Prezzo Totale: <?php echo($templateParams["prezzo-totale"]) ?>€</p>
-   <a class="confirm-button" href="#">Procedi all'ordine</a>
+  <h1>Riepilogo</h1>
+  <p class="lead fw-bold"> Numero Articoli: <?php echo ($templateParams["numero-articoli"]) ?><br />Prezzo Totale:
+    <?php echo ($templateParams["prezzo-totale"]) ?>€</p>
+  <a class="confirm-button" href="#">Procedi all'ordine</a>
 </section>
