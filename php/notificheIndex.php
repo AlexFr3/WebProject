2af0b97d928856419144c840740e3b639ff4169f
Alessandro Ambrogiani <alessandr.ambrogian4@studio.unibo.it>
2025-01-22 16:18:16 +0100

    added notifiche.php

---
+++ php/notificheIndex.php
@@ -0,0 +1,8 @@
+<?php 
+require_once 'bootstrap.php';
+
+$templateParams["titolo"] = "MangaParadise | Notifiche";
+$templateParams["nome"] = "notifiche.php";
+$templateParams["scripts"] = ["darkMode.js"];
+require 'template/base.php';
+?>
